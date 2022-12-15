ec6e032a2adfd0e457844a27634cf7190612561e
Ashwin Narayanan S <90165751+Ashrockzzz2003@users.noreply.github.com>
2022-12-15 14:31:01 +0530

    Create 121.py

---
+++ 121.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) <= 1:
+            return 0
+        
+        buy = prices[0]
+        profit = 0
+        prices = prices[1:]
+        for sell in prices:
+            if buy > sell:
+                buy = sell
+            elif sell - buy > profit:
+                profit = sell - buy
+        
+        
+        return profit
