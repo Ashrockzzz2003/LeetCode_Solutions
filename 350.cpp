a0c2fad509f3e4e4ef4886487fb9c9ba6ca73b40
Ashwin Narayanan S <90165751+Ashrockzzz2003@users.noreply.github.com>
2023-01-18 23:04:41 +0530

    intersection_of_two_arrays

---
+++ 350.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int left = 0, right = nums2.size();
+
+        vector<int> arr;
+
+        for(auto num: nums1) {
+            left = binarySearch(nums2, left, right, num);
+            if (left >= right) {
+                break;
+            }
+            if(nums2[left] == num) {
+                arr.push_back(nums2[left++]);
+            }
+        }
+
+        return arr;
+    }
+
+    int binarySearch(vector<int> nums, int lo, int hi, int target) {
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid;
+            }
+        }
+        return hi;
+    }
+};
