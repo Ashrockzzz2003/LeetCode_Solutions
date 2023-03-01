a682fe9ac507a0ddc197b47b7b157593a58e3677
Ashwin Narayanan S <90165751+Ashrockzzz2003@users.noreply.github.com>
2023-03-01 12:06:00 +0530

    Create 160.py

---
+++ 160.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if headA == headB:
+            return headA
+        
+        # Equalize the two lists
+        a = headA
+        b = headB
+
+        while a != b:
+            if a:
+                a = a.next
+            else:
+                a = headB
+            if b:
+                b = b.next
+            else:
+                b = headA
+        
+        return b
