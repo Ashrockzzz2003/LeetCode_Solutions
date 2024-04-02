4c70ea4473293b6d766ece453040bcaa17bed7f7
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2024-04-02 09:13:19 +0530

    isomorphic_strings

---
+++ 205.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> char_map;
+        unordered_map<char, char> map_done;
+
+        for(int i = 0; i < s.length(); i++) {
+            if (char_map.find(s[i]) == char_map.end() && (map_done.find(t[i]) == map_done.end() || map_done[t[i]] == false)) {
+                char_map[s[i]] = t[i];
+                map_done[t[i]] = true;
+            }
+        }
+
+        for(int i = 0; i < s.length(); i++) {
+            if (char_map[s[i]] != t[i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
