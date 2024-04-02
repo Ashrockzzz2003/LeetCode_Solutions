8dac78a0c344ca10fef5cf90ebff0ec50712989e
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2024-04-02 12:57:29 +0530

    type-fix

--- 205.cpp
+++ 205.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         unordered_map<char, char> char_map;
-        unordered_map<char, char> map_done;
+        unordered_map<char, bool> map_done;
 
         for(int i = 0; i < s.length(); i++) {
             if (char_map.find(s[i]) == char_map.end() && (map_done.find(t[i]) == map_done.end() || map_done[t[i]] == false)) {
