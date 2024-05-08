87d4d5b7802bd1bfb2005dc350803a33e089cec0
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2024-05-08 21:46:41 +0530

    relativeRanks HEAP approach

--- 506.cpp
+++ 506.cpp
@@ -1,32 +1,54 @@
 class Solution {
 public:
-    string getRankName(int rank) {
-        switch (rank) {
-            case 1: return "Gold Medal";
-            case 2: return "Silver Medal";
-            case 3: return "Bronze Medal";
-            default: return to_string(rank);
-        }
-    }
+    // string getRankName(int rank) {
+    //     switch (rank) {
+    //         case 1: return "Gold Medal";
+    //         case 2: return "Silver Medal";
+    //         case 3: return "Bronze Medal";
+    //         default: return to_string(rank);
+    //     }
+    // }
 
     vector<string> findRelativeRanks(vector<int>& score) {
-        unordered_map<int, int> rank;
-
         int n = score.size();
-        vector<int> sorted_array = score;
-
-        sort(sorted_array.begin(), sorted_array.end());
-
-        for(int i = n - 1; i >= 0; i--) {
-            rank[sorted_array[i]] = n - i;
-        }
-
-        vector<string> final_rank;
-
+        priority_queue<pair<int, int>> heap;
         for(int i = 0; i < n; i++) {
-            final_rank.push_back(getRankName(rank[score[i]]));
+            heap.push({score[i], i});
         }
 
-        return final_rank;
+        vector<string> final_ranks(n);
+
+        int curr_rank = 1;
+
+        while (!heap.empty()) {
+            int original_index = heap.top().second;
+            heap.pop();
+            if (curr_rank == 1) final_ranks[original_index] = "Gold Medal";
+            else if (curr_rank == 2) final_ranks[original_index] = "Silver Medal";
+            else if (curr_rank == 3) final_ranks[original_index] = "Bronze Medal";
+            else final_ranks[original_index] = to_string(curr_rank);
+            
+            curr_rank++;
+        }
+
+        return final_ranks;
+        // unordered_map<int, int> rank;
+
+        // int n = score.size();
+        // vector<int> sorted_array = score;
+
+        // sort(sorted_array.begin(), sorted_array.end());
+
+        // for(int i = n - 1; i >= 0; i--) {
+        //     rank[sorted_array[i]] = n - i;
+        // }
+
+        // vector<string> final_rank;
+
+        // for(int i = 0; i < n; i++) {
+        //     final_rank.push_back(getRankName(rank[score[i]]));
+        // }
+
+        // return final_rank;
     }
 };
