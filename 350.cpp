ffadca9da522af2c9afad53d86f167f421b0864c
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2024-07-02 22:31:53 +0530

    intersect

--- 350.cpp
+++ 350.cpp
@@ -34,3 +34,32 @@ public:
         return hi;
     }
 };
+
+/*
+Another approach:
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int i = 0, j = 0;
+        vector<int> intersection;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                intersection.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        return intersection;
+    }
+};
+*/
